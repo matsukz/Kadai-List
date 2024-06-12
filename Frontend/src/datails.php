50784b1c4eb0e0d0cef20fafdbba2ed8bfe95775
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-06-12 22:48:22 +0900

    インデント調整

--- Frontend/src/datails.php
+++ Frontend/src/datails.php
@@ -8,22 +8,20 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </head>
     <body>
-    <br>
-
-    <?php
-        //APIエンドポイント(本番環境では実際のドメインにする)
-        $Schema = "http://";
-        $Domain = "fastapi";
-        $fastapi = $Schema.$Domain.":9004/api/kadai/".$_POST['kadai_id'];
-        $data = "";
-        $data = json_decode(file_get_contents($fastapi),true);
-        
-        //タイトル
-        foreach ($data as $value){
-            echo "<h1 class='text-center'>".$value["title"]."</h1>";
-        }
-        
-    ?>
+        <br>
+        <?php
+            //APIエンドポイント(本番環境では実際のドメインにする)
+            $Schema = "http://";
+            $Domain = "fastapi";
+            $fastapi = $Schema.$Domain.":9004/api/kadai/".$_POST['kadai_id'];
+            $data = "";
+            $data = json_decode(file_get_contents($fastapi),true);
+            
+            //タイトル
+            foreach ($data as $value){
+                echo "<h1 class='text-center'>".$value["title"]."</h1>";
+            }
+        ?>
 
     </body>
 </html>
