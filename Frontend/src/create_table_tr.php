9297640d01eadbae93bba71279f0c03d4f2f799e
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-06-12 22:13:28 +0900

    残日数がわかりやすくなったと思う

--- Frontend/src/create_table_tr.php
+++ Frontend/src/create_table_tr.php
@@ -11,13 +11,26 @@
     //出力
     $html = "";
 
+    //今日の日付を取得する
+    $today = new DateTime();
+
     foreach($data as $value) {
+
+        //差を計算
+        $date = new DateTime($value["limit_date"]);
+        $interval = $today -> diff($date);
+        $limit_html = "";
+        if($today < $date){
+            $limit_html ="<td>".$interval->days."日</td>\n";          
+        } else {
+            $limit_html ="<td>期限切れ</td>\n";
+        }        
 
         $html.="<tr>\n";
         $html.="<td>".$value["group"]."</td>\n";
         $html.="<td>".$value["title"]."</td>\n";
         $html.="<td>".$value["limit_date"]."</td>\n";
-        $html.="<td>X日</td>\n";
+        $html.=$limit_html;
 
         if($value["status"] == 0){
             $html.="<td>未提出</td>\n";
