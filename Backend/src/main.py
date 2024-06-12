14167a422dd853f9c2c5d40fe373afdeba069419
まつ <92625267+matsukz@users.noreply.github.com>
2024-06-12 11:56:03 +0900

    DBをクエリするテスト

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 from connect_db import engine, database, get_db
+from kadai_model import Kadai
 
 app = FastAPI()
 
@@ -17,6 +17,21 @@ app.add_middleware(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+#起動時にDBと接続
+@app.on_event("startup")
+async def startup():
+  await database.connect()
+
+#終了時にDBから切断
+@app.on_event("shutdown")
+async def shutdown():
+  await database.disconnect()
+
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
+
+@app.get("/kadai/getall/")
+async def kadai_getall(db: Session=Depends(get_db)):
+  kadai = db.query(Kadai).all()
+  return kadai
