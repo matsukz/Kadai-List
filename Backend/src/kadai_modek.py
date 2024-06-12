c348e5deb2a3b77d7c3e92acde6a0b27987805c1
まつ <92625267+matsukz@users.noreply.github.com>
2024-06-12 11:42:48 +0900

    モデルを追加しました

--- Backend/src/kadai_modek.py
+++ Backend/src/kadai_modek.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, BigInteger ,String, Date
 from connect_db import Base
 
 class Kadai(Base):
     __tablename__ = "kadai"
-    id 
+    id = Column(BigInteger, primary_key=True, index=True)
+    resist_date = Column(Date)
+    start_date = Column(Date)
+    limit_date = Column(Date)
+    group = Column(String(length=100, collation='utf8mb3_general_ci'))
+    title = Column(String(length=100, collation='utf8mb3_general_ci'))
+    content = Column(String(length=100, collation='utf8mb3_general_ci'))
+    note = Column(String(length=100, collation='utf8mb3_general_ci'))
+    status = Column(Integer)
