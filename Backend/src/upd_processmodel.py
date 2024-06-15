f5f0d3c76fae316a7b964de38427692c31771d5b
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-06-15 15:39:21 +0900

    ステータスを真偽値にした

--- Backend/src/upd_processmodel.py
+++ Backend/src/upd_processmodel.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel
 from typing import Optional
 
 class Process(BaseModel):
-    status: int
+    status: bool
--- mysql/scripts/create.sql
+++ mysql/scripts/create.sql
@@ -7,6 +7,6 @@ CREATE TABLE `kadai` (
     `title` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
     `content` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
     `note` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
-    `status` int UNSIGNED NOT NULL DEFAULT 0,
+    `status` BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
