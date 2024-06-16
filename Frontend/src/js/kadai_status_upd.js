a90ebe5668f05551b12d449ea3d34053a54f154d
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-06-16 09:59:11 +0900

    APIを叩くAJAXできた

--- Frontend/src/js/kadai_status_upd.js
+++ Frontend/src/js/kadai_status_upd.js
@@ -5,8 +5,10 @@ document.getElementById("btn-success").onclick = function() {
 
     //ボタンを操作不可にする
     $("#btn-success").prop("disabled", true);
+    $("#btn-update").prop("disabled", true);
+    $("btn-delete").prop("disabled", true);
     //ボタンのテキストを変更する
-    $("#btn-successr").text("処理中...");
+    $("#btn-success").text("処理中...");
 
     //id取得
     const kadai_id = document.getElementById("kadai-id").value;
@@ -28,18 +30,29 @@ document.getElementById("btn-success").onclick = function() {
         return
     }
 
-    //送信するjsonデータの用意↓消す
-    var status_data = {
-        "status": status_flag
-    }
-    const put_date = JSON.stringify(status_data);
-
     //APIサーバー
     var url = new URL(window.location.href);
     var host = url.hostname;
 
     //状況更新APIを叩く
-    // $.ajax({
-    //     url: "http://" + host + ":9004/api/kadai/progress/" + kadai_id
-    // })
+    $.ajax({
+        url: "http://" + host + ":9004/api/kadai/process/" + kadai_id,
+        type: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify({"status": status_flag}),
+        cache: false
+    }).done(function(response){
+        alert("更新が完了しました。");
+        window.location.href = "index.php";
+    }).fail(function(xhr, status, error){
+        alert("更新に失敗しました。\n APIサーバを確認してください。");
+        console.error('AJAX Error:', status, error);
+    }).always(function(){
+        //ボタンを有効に戻す
+        $("#btn-success").prop("disabled", false);
+        $("#btn-update").prop("disabled", false);
+        $("btn-delete").prop("disabled", false);
+        //ボタンのテキストを変更する
+        $("#btn-successr").text(button_text);
+    })
 }
