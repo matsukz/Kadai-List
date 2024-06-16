56386a2ed2483c363f7d17d884df85923456f373
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-06-16 12:54:32 +0900

    deleteを投げるjs

---
+++ Frontend/src/js/kadai_delete.js
@@ -0,0 +1,40 @@
+document.getElementById("btn-delete").onclick = function() {
+    //ボタンのテキストを取得する
+    const button_text = document.getElementById("btn-delete").textContent;
+
+    //ボタンを操作不可にする
+    $("#btn-success").prop("disabled", true);
+    $("#btn-update").prop("disabled", true);
+    $("btn-delete").prop("disabled", true);
+    //ボタンのテキストを変更する
+    $("#btn-delete").text("処理中...");
+
+    //id取得
+    const kadai_id = document.getElementById("kadai-id").value;
+
+    //APIサーバー
+    var url = new URL(window.location.href);
+    var host = url.hostname;
+
+    //状況更新APIを叩く
+    $.ajax({
+        url: "http://" + host + ":9004/api/kadai/" + kadai_id,
+        type: "DELETE",
+        contentType: "application/json",
+        data: JSON.stringify({"id": kadai_id}),
+        cache: false
+    }).done(function(response){
+        alert(kadai_id + "を削除しました。");
+        window.location.href = "index.php";
+    }).fail(function(xhr, status, error){
+        alert("削除に失敗しました。\n APIサーバを確認してください。");
+        console.error('AJAX Error:', status, error);
+    }).always(function(){
+        //ボタンを有効に戻す
+        $("#btn-success").prop("disabled", false);
+        $("#btn-update").prop("disabled", false);
+        $("btn-delete").prop("disabled", false);
+        //ボタンのテキストを変更する
+        $("#btn-delete").text(button_text);
+    })
+}
