dc7ccdf2cbc639a19602057707067bc934155675
まつ <92625267+matsukz@users.noreply.github.com>
2024-07-06 13:38:52 +0900

    タイムアウトの設定

--- Frontend/src/call_api.php
+++ Frontend/src/call_api.php
@@ -4,13 +4,17 @@
         $ch = curl_init();
         $curl_options = array(
             CURLOPT_URL => $api_point,
-            CURLOPT_RETURNTRANSFER, true,
-            CURLOPT_FOLLOWLOCATION, true
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_TIMEOUT => 10
         );
         curl_setopt_array($ch,$curl_options);
 
         $response = curl_exec($ch); //curl実行
         $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        //タイムアウト
+        if (curl_error($ch)) { return 444; }
 
         //レスポンスによって処理
         if($http_code == 200){
