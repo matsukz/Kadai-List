92df372d14509125e1349a8696c68ccd95513925
まつ <92625267+matsukz@users.noreply.github.com>
2024-07-06 13:16:21 +0900

    cURLを実行する関数を作成

---
+++ Frontend/src/call_api.php
@@ -0,0 +1,22 @@
+<?php
+    function($api_point){
+
+        $ch = curl_init();
+        $curl_options = array(
+            CURLOPT_URL => $api_point,
+            CURLOPT_RETURNTRANSFER, true,
+            CURLOPT_FOLLOWLOCATION, true
+        );
+        curl_setopt_array($ch,$curl_options);
+
+        $response = curl_exec($ch); //curl実行
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        //レスポンスによって処理
+        if($http_code == 200){
+            return json_decode($response, true);
+        } else {
+            return $http_code;
+        }
+    }
+?>
