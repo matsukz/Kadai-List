a16ef4814f44d834b1680686d62bc9c7417006af
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-07-07 00:46:00 +0900

    ロードを一度だけにした

--- Frontend/src/index.php
+++ Frontend/src/index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- スマホ版に対応させる -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js""></script>
-        <?php include("create_table_tr.php"); ?>
+        <?php include_once("create_table_tr.php"); ?>
     </head>
     <body>
 
