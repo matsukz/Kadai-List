1b633d4b9c9f1e8fdcaa6ac223f7918ec53adc0f
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-07-07 22:57:25 +0900

    自動列の設定を変えた

--- mysql/scripts/create.sql
+++ mysql/scripts/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `kadai` (
-    `kadai_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+    `kadai_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     `register_date` date NOT NULL DEFAULT '2000-01-01',
     `start_date` date NOT NULL DEFAULT '2000-01-01',
     `limit_date` date NOT NULL DEFAULT '2000-01-01',
@@ -8,14 +8,14 @@ CREATE TABLE `kadai` (
     `content` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
     `note` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
     `status` BOOLEAN NOT NULL DEFAULT FALSE,
-    `user_id` INT NOT NULL AUTO_INCREMENT
+    `user_id` INT NOT NULL,
     PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `users` (
-    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     `username` VARCHAR(50) NOT NULL UNIQUE,
     `password_hash` VARCHAR(100) NOT NULL,
-    `api_key` VARCHAR(100) NOT NULL UNIQUE
+    `api_key` VARCHAR(100) NOT NULL UNIQUE,
     PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
