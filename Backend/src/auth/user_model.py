f61940650499faa023ff6cca864d4b5d7b6edebb
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-07-07 23:28:10 +0900

    カラム名が間違ってた

--- Backend/src/auth/user_model.py
+++ Backend/src/auth/user_model.py
@@ -7,7 +7,7 @@ from connect_db import Base
 #ユーザーに関する情報
 class Users(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True)
     password_hash = Column(String(100))
     api_key = Column(String(100), unique=True)
