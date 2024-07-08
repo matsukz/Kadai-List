c2391b8adbf1265c32c8042dffbaf856f4d2624b
まつ <92625267+matsukz@users.noreply.github.com>
2024-07-08 14:04:46 +0900

    トークン確認関数の追加

--- Backend/src/auth/auth.py
+++ Backend/src/auth/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -10,7 +10,7 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
 
-from auth.user_model import Users, UserCreate
+from auth.user_model import Users, UserCreate,TokenData
 from connect_db import get_db
 
 load_dotenv()
@@ -61,9 +61,26 @@ def authenticate_user(username:str, password:str, db: Session=Depends(get_db)):
         return False
     return user
 
-
 def get_user(username:str, db: Session=Depends(get_db)):
     return db.query(Users).filter(Users.username == username).first()
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+async def get_current_user(db: Session=Depends(get_db) ,token: str=Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None: raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    
+    user = get_user(db, username=token_data.username)
+    if user is None: raise credentials_exception
+    return user
