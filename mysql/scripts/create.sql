0ccf8e0edccb332c7fc84044ccc3ab313c96fad1
まつ <92625267+matsukz@users.noreply.github.com>
2024-07-10 19:58:12 +0900

    user_idを外部キーにした

--- mysql/scripts/create.sql
+++ mysql/scripts/create.sql
@@ -1,5 +1,13 @@
+CREATE TABLE `users` (
+    `user_id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password_hash` VARCHAR(100) NOT NULL,
+    `api_key` VARCHAR(100) NOT NULL UNIQUE,
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
+
 CREATE TABLE `kadai` (
-    `kadai_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    `kadai_id` INT NOT NULL AUTO_INCREMENT UNIQUE,
     `register_date` date NOT NULL DEFAULT '2000-01-01',
     `start_date` date NOT NULL DEFAULT '2000-01-01',
     `limit_date` date NOT NULL DEFAULT '2000-01-01',
@@ -11,12 +19,4 @@ CREATE TABLE `kadai` (
     `user_id` INT NOT NULL,
     PRIMARY KEY (`kadai_id`),
     FOREIGN KEY (`user_id`) REFERENCES users(`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
-
-CREATE TABLE `users` (
-    `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-    `username` VARCHAR(50) NOT NULL UNIQUE,
-    `password_hash` VARCHAR(100) NOT NULL,
-    `api_key` VARCHAR(100) NOT NULL UNIQUE,
-    PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
