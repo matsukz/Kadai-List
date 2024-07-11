31186a8c62fe4060fee8092b63841803f0850e40
まつ <92625267+matsukz@users.noreply.github.com>
2024-07-11 23:19:20 +0900

    Ajax追加

--- Frontend/src/js/login.js
+++ Frontend/src/js/login.js
@@ -14,6 +14,28 @@ document.getElementById("login").addEventListener("submit", function(event){
         return
     }
 
-    
-
-})
+    $.ajax({
+        type: "POST",
+        url: api_point + "auth/token",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: {"username": login_id, "password": login_password},
+        dataType: "josn",
+        cache: false
+    }).done(function(response){
+        alert("OK");
+        // const AccessToken = response.access_token;
+        // $.post(
+        //     "index.php", {token: AccessToken}, function() {
+        //         window.location.href = "index.php";
+        //     });
+    }).fail(function(jqXHR, textStatus) {
+        console.log("Login failed: " + jqXHR.responseText)
+        alert("ログインに失敗しました");
+    }).always(function(){
+        $("#Register").prop("disabled", false);
+        $("#Register").text("ログイン");
+    });
+});
