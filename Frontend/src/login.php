a793ac09796ec2eccceca22f0e83e040b744bae9
まつ <92625267+matsukz@users.noreply.github.com>
2024-07-17 11:49:15 +0900

    ログイン処理は全て別ファイルで

--- Frontend/src/login.php
+++ Frontend/src/login.php
@@ -1,10 +1,3 @@
-<?php
-    if($_SERVER["REQUEST_METHOD"] = "GET"){
-
-    } else {
-        
-    }
-?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -13,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- スマホ版に対応させる -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js""></script>
-        <?php include_once("create_table_tr.php"); ?>
+        <?php ?>
     </head>
     <body>
 
@@ -24,7 +17,7 @@
         <div class="d-flex justify-content-center">
             <div class="col-10 col-mb-12">
 
-                <form id="login" action="">
+                <form id="login">
                     <div class="mb-3 row">
                         <label class="col-sm-2 col-form-label">ID</label>
                         <div class="col-sm-10">
